2740316550a8bc82efa308e1ada478cd14dcccc5
Oracle Public Cloud User <opc@instance-20210722-1122.subnet.vcn.oraclevcn.com>
2021-07-28 07:46:28 +0000

    almost working?

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 conda activate atlas-GAN
 export CUDA_VISIBLE_DEVICES=1
-python train_script.py --name test-qsm --dataset qsm --oversample --nonorm_reg --clip --losswt_gp 5e-4 --gen_config ours --unconditional
+python train_script.py --name test-qsm --dataset qsm --nonorm_reg --clip --losswt_gp 5e-4 --gen_config ours
