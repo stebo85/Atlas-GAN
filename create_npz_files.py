a201c8a60d38264c166d1191eaa8dbcee599e7d6
Oracle Public Cloud User <opc@instance-20210722-1122.subnet.vcn.oraclevcn.com>
2021-07-29 05:42:48 +0000

    added age and gender

--- create_npz_files.py
+++ create_npz_files.py
@@ -6,6 +6,12 @@ files = ['ourdata/' + f for f in os.listdir('ourdata') if f.endswith('mn_skull_s
 
 target_path='/mnt/FileSystem-DeepLearning/vnm/qsm28_data/train_npz/'
 
+print(files)
+
+age = np.array([29,26,23,31,34,29,21,23,22,26,24,32,25,29,25,23,23,27,23,22,29,24,27,27,29,32,23,31,24])
+gender = np.array([0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1])
+
+
 if not os.path.exists(target_path):
     os.mkdir(target_path)
 
@@ -17,4 +23,6 @@ for file in files:
     np.savez_compressed(
         out_file,
         vol=npy_img,
+        age=age,
+        gender=gender,
     )
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 conda activate atlas-GAN
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python train_script.py --name test-qsm --dataset qsm --nonorm_reg --clip --losswt_gp 5e-4 --gen_config ours
